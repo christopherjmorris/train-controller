9518e5848958b72e7b299a2ad909e574189ff104
Chris Morris <chris.morris@ranplanwireless.com>
2018-07-26 13:39:34 +0100

    Fix speed visualiser basic implementation

--- pwm_controller/SpeedVisualiser.h
+++ pwm_controller/SpeedVisualiser.h
@@ -9,29 +9,57 @@ class SpeedVisualiser {
   public:
   
   SpeedVisualiser(int pinNeopixel) {
-    this->pinNeopixel = pinNeopixel;
-    strip = Adafruit_NeoPixel(12, pinNeopixel, NEO_GRB + NEO_KHZ400);
+    strip = Adafruit_NeoPixel(12, pinNeopixel, NEO_GRB + NEO_KHZ800);
 
     strip.begin();
-    strip.show();
-    
   }
   void update(ControllerState state) {
-    unsigned char speedMsb = state.speed >> 5;
-    unsigned char speedLower = (state.speed & 0b00011111) << 2;
-    for (int index = 0; index < 8; index++) {
-      if (index < speedMsb) {
-        strip.setPixelColor(index, 0, 128, 0, 0);
-      } else if (index == speedMsb) {
-        strip.setPixelColor(index, 0, speedLower, 0, 0);
-      }
+
+    if (state.direction == DIRECTION_STOPPED) {
+      ShowRedRing();
+      return;
+    }
+    
+    ShowPartialGreen(state.speed);
+  }
+  
+  private:
+
+  void ShowNothing() {
+    for (int index = 0; index < strip.numPixels(); index++) {
+      strip.setPixelColor(index, 0, 0, 0, 0);
     }
     strip.show();
   }
   
-  private:
+  void ShowRedRing() {
+    for (int index = 0; index < strip.numPixels(); index++) {
+      strip.setPixelColor(index, 64, 0, 0, 0);
+    }
+    strip.show();
+  }
+
+  void ShowPartialGreen(unsigned char speed) {
+    unsigned char speedMsb = speed >> 5;
+    unsigned char speedLower = (speed & 0b00011111) << 1;
+    int index;
+    for (index = 0; index < 8; index++) {
+      if (index < speedMsb) {
+        strip.setPixelColor(index, 0, 64, 0, 0);
+      } else if (index == speedMsb) {
+        strip.setPixelColor(index, 0, speedLower, 0, 0);
+      }
+      else {
+        strip.setPixelColor(index, 0, 0, 0, 0);
+      }
+    }
+    for (; index < strip.numPixels(); index++) {
+      strip.setPixelColor(index, 0, 0, 0, 0);
+    }
+    
+    strip.show();
+  }
   
-  int pinNeopixel;
   Adafruit_NeoPixel strip;
 };
 
